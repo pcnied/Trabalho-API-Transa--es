c901979f12360277040ec361b9ae5f592f659068
Peterson C C Nied <petersonied@gmail.com>
2023-09-20 22:35:17 -0300

    Atualizar atividade

--- src/usecase/transaction/createTransaction.usecase.ts
+++ src/usecase/transaction/createTransaction.usecase.ts
@@ -1,14 +1,14 @@
 import Transaction, { TypeTransaction } from "../../classes/transaction";
 import UserRepository from "../../repositories/user/user.repository";
 
-export type CreateTransactionRequestDTO = {
+export type CreateAnotationRequestDTO = {
   userId: string;
   title: string;
   value: number;
   type: TypeTransaction;
 };
 
-export type CreateTransactionResponseDTO = {
+export type CreateAnotationResponseDTO = {
   status: string;
   transaction: Transaction;
 };
@@ -16,14 +16,14 @@ export type CreateTransactionResponseDTO = {
 class CreateTransactionUseCase {
   constructor(private userRepository: UserRepository) {}
 
-  execute(data: CreateTransactionRequestDTO): CreateTransactionResponseDTO {
+  execute(data: CreateAnotationRequestDTO): CreateAnotationResponseDTO {
     const { userId, title, value, type } = data;
 
     const transaction = new Transaction(title, value, type);
     this.userRepository.createTransaction(userId, transaction);
 
     return {
-      status: "Transação criada e adicionada com sucesso!",
+      status: "Anotação criada e adicionada com sucesso!",
       transaction,
     };
   }
